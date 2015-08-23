eaa7217f2d2e6a35a5bbdf327bc82dbf40331000
gkumar10 <gaurav.kumar@me.com>
2015-08-23 10:44:39 -0700

    ..

--- shinyapp/server.R
+++ shinyapp/server.R
@@ -12,14 +12,14 @@ shinyServer(
           predictedwords <- nextword(input$sentence)
           HTML('<b>Best choice of next word:</b> <br>',
              ifelse(is.na(predictedwords[1]), "No prediction of next word. Please try a different phrase.", predictedwords[1]), '<br><br>',
-             ifelse(is.na(predictedwords[2]), "", '<b>Other choices (optional):</b> <br>'),
-             ifelse(is.na(predictedwords[2]), "", predictedwords[2]), '<br>',
-             ifelse(is.na(predictedwords[3]), "", predictedwords[3]), '<br>',
-             ifelse(is.na(predictedwords[4]), "", predictedwords[4]), '<br>',
-             ifelse(is.na(predictedwords[5]), "", predictedwords[5]), '<br>',
-             ifelse(is.na(predictedwords[6]), "", predictedwords[6]) 
+             ifelse(is.na(predictedwords[2]) | !input$otherwords, "", '<b>Other word choices:</b> <br>'),
+             ifelse(is.na(predictedwords[2]) | !input$otherwords, "", predictedwords[2]), '<br>',
+             ifelse(is.na(predictedwords[3]) | !input$otherwords, "", predictedwords[3]), '<br>',
+             ifelse(is.na(predictedwords[4]) | !input$otherwords, "", predictedwords[4]), '<br>',
+             ifelse(is.na(predictedwords[5]) | !input$otherwords, "", predictedwords[5]), '<br>',
+             ifelse(is.na(predictedwords[6]) | !input$otherwords, "", predictedwords[6]) 
         )
-        
+      
       }
    })
 })
--- shinyapp/ui.R
+++ shinyapp/ui.R
@@ -3,8 +3,9 @@ library(shiny)
 #shiny app for next word prediction
 #coursera data science capstone project
 
-text1 <- "Type a phrase with at least 1 word"
-text2 <- "Click 'Predict Next Word' button."
+text1 <- "Type a phrase with at least 1 word."
+text2 <- "Check the box to see other available choice of words, if any."
+text3 <- "Click 'Predict Next Word' button."
 
 shinyUI(
   pageWithSidebar(
@@ -16,9 +17,10 @@ shinyUI(
     sidebarPanel(
       h4("This Shiny app will predict next word in your phrase."),
       helpText(strong("Instructions:")),
-      helpText(strong(tags$ol(tags$li(text1), tags$li(text2)))),
+      helpText(strong(tags$ol(tags$li(text1), tags$li(text2), tags$li(text3)))),
       h2("Input:"),
       textInput("sentence", label="Phrase", value=""),
+      checkboxInput("otherwords", label="Include other word choices? (if available)", value=FALSE),
       submitButton('Predict Next Word')
       ),
     
